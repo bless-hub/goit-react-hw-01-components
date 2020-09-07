d383fbd1f9c1a58731314d70a2df53318f94760e
Vladyslav Bondarenko <bvbasys@gmail.com>
2020-09-07 20:56:29 +0300

    Updates

---
+++ precache-manifest.9aa28932a70cc1225cb62adf3c978ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930139525e27ce2c3bdd5e570820051d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c583ca506677dfe10cda",
+    "url": "/static/css/main.cfc704bd.chunk.css"
+  },
+  {
+    "revision": "708913c93c7cf3229840",
+    "url": "/static/js/2.b0c749eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b0c749eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c583ca506677dfe10cda",
+    "url": "/static/js/main.8c2fe554.chunk.js"
+  },
+  {
+    "revision": "a2a41e8544dfc1d8c537",
+    "url": "/static/js/runtime-main.edf9d2ae.js"
+  }
+]);
